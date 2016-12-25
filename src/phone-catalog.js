cdcbea4a22a466620886636db68886dc37f381f8
PavelOfficial <pavelofficial@ya.ru>
2016-12-25 17:13:35 +0300

    webpack application build

---
+++ src/phone-catalog.js
@@ -0,0 +1,6 @@
+// Стили.
+import './asserts/styles.css';
+
+import angular from 'angular';
+
+angular.module('phoneCatalog', []);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
 'use strict';
 
+var NODE_ENV = process.env.NODE_ENV || 'development';
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var __DEVELOPMENT__ = NODE_ENV !== 'production';
+var path = require('path');
+
 module.exports = {
-    entry: './src/scripts/main.js',
-    resolve: {
-        alias: {
-            createjs: 'PreloadJS/lib/preloadjs-0.6.2.combined.js'
-        }
+    context: path.join(__dirname, '/src'),
+    entry: './phone-catalog.js',
+    watch: true,
+    output: {
+        path: path.join(__dirname, '/app'),
+        publicPath: '/',
+        filename: 'build.js'
     },
     module: {
         loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'ng-annotate!babel' },
-            { test: /\.json$/, loader: 'json' },
-            { test: /.*gsap.*/, loader: 'imports?gs=>window.GreenSockGlobals={}!exports?gs' },
-            { test: /.*PreloadJS.*/, loader: 'imports?this=>global!exports?window.createjs' }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, loader: 'ng-annotate!babel'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css?resolve url')
+            },
+            {
+                test: /\.(svg|ttf|eot|woff|woff2)$/,
+                loader: 'file?name=asserts/fonts/[name].[ext]'
+            } /*,
+            {
+                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                exclude: /\/node_modules\//,
+                loader: 'file?name=[name].[ext]'
+            }*/
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('./asserts/styles.css')
+    ]
 };
