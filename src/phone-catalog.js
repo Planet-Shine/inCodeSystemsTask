d1b77338c6e269c10f3cadc39e164b651c59383a
PavelOfficial <pavelofficial@ya.ru>
2017-01-04 15:27:37 +0300

    components catalog

--- src/phone-catalog.js
+++ src/phone-catalog.js
@@ -2,14 +2,14 @@ import angular from 'angular';
 import 'angular-ui-router';
 
 // Components
-import './phone-list/phone-list.component';
-import './phone-details/phone-details.component';
-import './phone-claim-form/phone-claim-form.component';
-import './hierarchic-categories/hierarchic-categories.component';
-import './header/header.component';
-import './phone-catalog-app/phone-catalog-app.component';
-import './image-gallery/image-gallery.component';
-import './float-panel/float-panel.component';
+import './components/phone-list/phone-list.component';
+import './components/phone-details/phone-details.component';
+import './components/phone-claim-form/phone-claim-form.component';
+import './components/hierarchic-categories/hierarchic-categories.component';
+import './components/header/header.component';
+import './components/phone-catalog-app/phone-catalog-app.component';
+import './components/image-gallery/image-gallery.component';
+import './components/float-panel/float-panel.component';
 import './core/core.module';
 
 export default angular.module('phoneCatalog', [
